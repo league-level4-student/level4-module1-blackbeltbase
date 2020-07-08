3ab0584a36edabfd1fb84971f02b8e644a642141
blackbeltbase <54255614+blackbeltbase@users.noreply.github.com>
2020-07-08 13:28:22 -0700

    CustomArrayList, Custom Button Option, Intro to Enums, Horoscope(IP)

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -3,34 +3,72 @@ package _01_Custom_ArrayList;
 @SuppressWarnings("unchecked")
 
 public class ArrayList <T>{
-	
+	T[] array;
 	public ArrayList() {
-		
+     array = (T[])new Object[0];
 	}
 	
 	public T get(int loc) throws IndexOutOfBoundsException {
-		
-		return null;
+		return array[loc];
 	}
 	
 	public void add(T val) {
-		
+				T[] secondArray = (T[])new Object[array.length+1];
+				secondArray[secondArray.length-1] = val;
+				for(int i = 0; i<array.length;i++) {
+					secondArray[i] = array[i];
+				}
+				array = secondArray;
 	}
 	
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
-		
+		T[] newStuff = (T[]) new Object[array.length+1];
+		for(int i = 0; i<newStuff.length;i++) {
+			if(i<loc) {
+				newStuff[i] = array[i];
+			}
+			else if(i>loc) {
+				newStuff[i] = array[i-1];
+			}
+			else {
+				newStuff[i] = val;
+			}
+		}
+		array = newStuff;
 	}
 	
 	public void set(int loc, T val) throws IndexOutOfBoundsException {
-		
+		array[loc] = val;
 	}
 	
 	public void remove(int loc) throws IndexOutOfBoundsException {
+T[] newStuff = (T[]) new Object[array.length-1] ;
+				
 		
+		//B. make a for loop to iterate through the member array
+		for(int i = 0; i<array.length;i++) {
+		if(i<loc) {
+			newStuff[i] = array[i];
+		}
+		else if(i>loc) {
+			newStuff[i-1] =array[i];
+		}
+		else {
+		}
+		}
+		array = newStuff;
 	}
 	
 	public boolean contains(T val) {
-		
+		for(int i = 0; i<array.length;i++) {
+			if(array[i] == val) {
+				return true;
+			}
+		}
 		return false;
+	}
+
+	public int size() {
+		return array.length;
 	}
 }
--- src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
+++ src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
@@ -1,17 +1,93 @@
 package _03_Switch_Statement_Practice;
 
+import java.net.URI;
+
 import javax.swing.JOptionPane;
 
 public class CustomButtonOptionPanes {
-	public static void main(String[] args) {
+public static void main(String[] args) {
+	CustomButtonOptionPanes memes = new CustomButtonOptionPanes();
+}
+	public CustomButtonOptionPanes() {
 		String[] options = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
 
 		int input = JOptionPane.showOptionDialog(null, "Choose a day of the week", "Custom Buttons", 0, -1, null,
 				options, 0);
 
 		String choice = options[input];
-		
+		playStuff(choice);
 		//use a switch statement to do something cool for each option
 		
+		
 	}
+	 void playStuff(String choice) {
+		switch(choice) {
+		case "Sunday": 
+			showSunday();
+			break;
+		case "Monday": 
+			showMonday();
+			break;
+		case "Tuesday": 
+			showTuesday();
+			break;
+		case"Wednesday":
+			 showWednesday();
+			 break;
+		case"Thursday":
+			showThursday() ;
+			break;
+		case"Friday":
+			showFriday();
+			break;
+		case"Saturday":
+			showSaturday();
+			break;
+		default: 
+			System.out.println("Something went wrong :(");
+		
+		}
+	}
+	 void showSunday() {
+	     playVideo("https://www.youtube.com/watch?v=KQetemT1sWc");
+	}
+
+	void showMonday() {
+	     playVideo("https://www.youtube.com/watch?v=SsmVgoXDq2w");
+	}
+
+	void showTuesday() {
+	     playVideo("https://www.youtube.com/watch?v=CY8a4uh_PdI");
+	}
+	void showWednesday() {
+	     playVideo("https://www.youtube.com/watch?v=PE8GlPpuLuY");
+	}
+	void showThursday() {
+	     playVideo("https://www.youtube.com/watch?v=3IxkRXHfslQ");
+	}
+	void showFriday() {
+	     playVideo("https://www.youtube.com/watch?v=qFoEBIfVj9M");
+	}
+	void showSaturday() {
+	     playVideo("https://www.youtube.com/watch?v=KLrZmDIh8_0");
+	}
+
+	void playVideo(String videoID) {
+	     
+	     // Workaround for Linux because "Desktop.getDesktop().browse()" doesn't
+	     //work on some Linux implementations
+	     try {
+	     if (System.getProperty("os.name").toLowerCase().contains("linux")) {
+	     if (Runtime.getRuntime().exec(new String[] {
+	      "which", "xdg- open" }).getInputStream().read() != -1) {
+	     Runtime.getRuntime().exec(new String[] { "xdg-open", videoID });
+	     }
+	     } else {
+	          URI uri = new URI(videoID);
+	          java.awt.Desktop.getDesktop().browse(uri);
+	     }      } catch (Exception e) {
+	          e.printStackTrace();
+	     }
+	}
+
 }
